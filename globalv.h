99e601faf2b6f57ad5d9ec839546c8b9dfe405a5
huangt229 <htmwp@outlook.com>
2022-04-19 10:53:45 +0800

    Enter

---
+++ globalv.h
@@ -0,0 +1,12 @@
+#ifndef _globalv__h
+#define _globalv__h
+double_t sin2TW = 0.23857;
+double_t a = 0.7e-13;
+double_t r0 = 1.3e-13;
+double_t Np = 14;
+double_t Nn = 14;
+double_t hbar = 197.327;/*Mev-fm*/
+double_t GF = 1.166378e-5;/*fermi constant*/
+double_t Na = 6.022e23;/*Avogadro number*/
+double_t Mamu = 931.494;/*Mev atomic mass unit*/
+#endif
